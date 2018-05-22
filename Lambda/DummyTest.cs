4bb25dc9fbd2a3d64027d1dd182111c647fba358
Martini Arialdo <arialdo.martini@tcpos.com>
2018-05-22 12:42:19 +0200

    An empty project with a dummy, passing test

---
+++ Lambda/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Lambda
+{
+  public class DummyTest
+  {
+    [Fact]
+    public void should_pass()
+    {
+      "friends".Should().Be("friends");
+    }
+  }
+}
